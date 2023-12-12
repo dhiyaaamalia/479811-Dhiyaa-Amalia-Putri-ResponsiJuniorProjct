9c07742dd32868ccecd8fe383d9ec0835b871145
Dhiyaa Amalia P <93065032+dhiyaaamalia@users.noreply.github.com>
2023-12-12 12:10:21 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Responsi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNamaKaryawan = new System.Windows.Forms.TextBox();
+            this.cbDepKaryawan = new System.Windows.Forms.ComboBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama Karyawan : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dep. Karyawan : ";
+            // 
+            // txtNamaKaryawan
+            // 
+            this.txtNamaKaryawan.Location = new System.Drawing.Point(178, 80);
+            this.txtNamaKaryawan.Name = "txtNamaKaryawan";
+            this.txtNamaKaryawan.Size = new System.Drawing.Size(169, 20);
+            this.txtNamaKaryawan.TabIndex = 2;
+            // 
+            // cbDepKaryawan
+            // 
+            this.cbDepKaryawan.FormattingEnabled = true;
+            this.cbDepKaryawan.Items.AddRange(new object[] {
+            "HR",
+            "ENG",
+            "DEV",
+            "PM",
+            "FIN"});
+            this.cbDepKaryawan.Location = new System.Drawing.Point(178, 111);
+            this.cbDepKaryawan.Name = "cbDepKaryawan";
+            this.cbDepKaryawan.Size = new System.Drawing.Size(169, 21);
+            this.cbDepKaryawan.TabIndex = 3;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(70, 173);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(84, 27);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(205, 173);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(84, 27);
+            this.btnEdit.TabIndex = 6;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(334, 173);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(84, 27);
+            this.btnHapus.TabIndex = 7;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(106, 220);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(415, 186);
+            this.dgvData.TabIndex = 8;
+            this.dgvData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvData_CellContentClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "ID Departemen:",
+            "HR : HR",
+            "ENG : Engineer",
+            "DEV : Developer",
+            "PM : Product Manager",
+            "FIN : Finance"});
+            this.listBox1.Location = new System.Drawing.Point(430, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(273, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "LOGO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.cbDepKaryawan);
+            this.Controls.Add(this.txtNamaKaryawan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNamaKaryawan;
+        private System.Windows.Forms.ComboBox cbDepKaryawan;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Net;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Responsi
+{
+    public partial class Form1 : Form
+    {
+        private NpgsqlConnection conn;
+        string connstring = "Host=localhost;Port=5432;Username=postgres;Password=informatika;Database=lia_responsi";
+        public DataTable dt;
+        private string sql = null;
+        private DataGridViewRow r;
+        public static NpgsqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new NpgsqlConnection(connstring);
+            RefreshData();
+        }
+        void RefreshData()
+        {
+            try
+            {
+                conn.Open();
+                dgvData.DataSource = null;
+                sql = @"SELECT*from kr_select()";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                NpgsqlDataReader rd = cmd.ExecuteReader();
+                dt.Load(rd);
+                dgvData.DataSource = dt;
+                conn.Close();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Error: " + ex.Message, "FAIL!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                sql = @"Select * from kr_insert(: _nama, : id_dep)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_nama", txtNamaKaryawan.Text);
+                cmd.Parameters.AddWithValue("_id_dep", cbDepKaryawan.SelectedItem.ToString());
+                if((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Data karyawan berhasil diinputkan", "informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                    RefreshData();
+                    txtNamaKaryawan.Text = cbDepKaryawan.Text = null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Insert FAIL!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }   
+        }
+
+        private void dgvData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0)
+            {
+                r = dgvData.Rows[e.RowIndex];   
+                txtNamaKaryawan.Text = r.Cells["_nama"].Value.ToString();
+                cbDepKaryawan.Text = r.Cells["_id_dep"].Value.ToString();
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (r == null)
+            {
+                MessageBox.Show("Pilih Baris data yang akan di edit","Good", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            try
+            {
+                conn.Open();
+                sql = @"Select * from kr_update(: _nama,:_id_dep,:_id_karyawan)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id_karyawan", r.Cells["_id_karyawan"].Value.ToString());
+                cmd.Parameters.AddWithValue("_nama",txtNamaKaryawan.Text);
+                cmd.Parameters.AddWithValue("_id_dep", cbDepKaryawan.Text);
+                if ((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Data karyawan berhasil diedit", "Good", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                    RefreshData();
+                    txtNamaKaryawan.Text = cbDepKaryawan.Text = null;
+                    r = null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Edit FAIL!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            if(r == null)
+            {
+                MessageBox.Show("Pilh barus yang akan dihapus","Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if(MessageBox.Show("Apakah benar anda ingin menghapus data karyawan" + r.Cells["_nama"].Value.ToString()+"?","Konfirmasi terhapus",MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1)==DialogResult.Yes)
+                try
+                {
+                    conn.Open();
+                    sql = @"Select * from kr_delete(:_id_karyawan)";
+                    cmd = new NpgsqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("_id_karyawan", r.Cells["_id_karyawan"].Value.ToString());
+                    if ((int)cmd.ExecuteScalar() == 1)
+                    {
+                        MessageBox.Show("Data karyawan berhasil dihapus", "Good", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        conn.Close();
+                        RefreshData();
+                        txtNamaKaryawan.Text = cbDepKaryawan.Text = null;
+                        r = null;
+                    }
+
+                }
+                catch (Exception ex) 
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Delete FAIL!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+        }
+    }
+}
